2047531a2d3dccb3a06726906ea8e3f1da7b52e6
Julia-Abramova <yulia.abramova14@yandex.ru>
2026-03-21 13:47:46 +0300

    new main

--- Sorcery Fight/src/main/java/org/example/Main.java
+++ Sorcery Fight/src/main/java/org/example/Main.java
@@ -9,22 +9,18 @@ import java.util.Scanner;
 import org.example.printer.MissionPrinter;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter path to mission file: ");
         String path = scanner.nextLine();
         File file = new File(path);
         if (!file.exists()) {
-            System.err.println("File not found: " + path);
+            System.out.println("File not found: " + path);
             return;
         }
 
-        try {
-            MissionParser parser = ParserFactory.getParser(file);
-            Mission mission = parser.parse(path);
-            MissionPrinter.print(mission);
-        } catch (IOException e) {
-            System.err.println("error: ");
-        }
+        MissionParser parser = ParserFactory.getParser(file);
+        Mission mission = parser.parse(path);
+        MissionPrinter.print(mission);
     }
 }
